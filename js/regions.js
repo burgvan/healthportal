1bb12b8327fb67f0415627dddd13653434a1520d
Vanessa Burguete <vanessa.burguete@gmail.com>
2017-04-25 20:53:37 -0400

    Health portal working prototype
    Linking this to my portfolio site.

---
+++ js/regions.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+/***************************
+This page is an example of an array you could use for the dropdown menus.
+Make sure you include this page before you include your main javascript file.
+
+Each array uses the same format XX:Name of Region Where XX is the abbreviation for the region
+In a drop down list the XX would be the value for the option and the Name would be the text displayed between the
+opening and closing option tags.
+***************************/
+
+var ON = ['OT:Ottawa', 'TO:Toronto'];
+
+var BC = ['VA:Vancouver', 'VI:Victoria'];
+
+var NB = ['FR:Fredricton', 'SJ:Saint-Jean'];
